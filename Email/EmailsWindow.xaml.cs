5106c38f3484b98826b0ee3b464e9454ebad2c83
Santiago Pintos <spintos@outlook.es>
2024-07-25 09:32:35 -0300

    refactor: remove unused deps

--- Email/EmailsWindow.xaml.cs
+++ Email/EmailsWindow.xaml.cs
@@ -1,21 +1,8 @@
 ﻿using Microsoft.Graph;
 using Microsoft.Graph.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Microsoft.Web.WebView2.Core;
-using Microsoft.Web.WebView2.Wpf;
 
 namespace Email
 {
